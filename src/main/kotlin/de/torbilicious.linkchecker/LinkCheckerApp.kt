66891b9f6aab7803693b6771e2fb6c965df1233b
Torbilicious <torben.binder@googlemail.com>
2018-01-10 01:04:56 +0100

    Providing trusted certificates (work in progress)

--- src/main/kotlin/de/torbilicious.linkchecker/LinkCheckerApp.kt
+++ src/main/kotlin/de/torbilicious.linkchecker/LinkCheckerApp.kt
@@ -3,16 +3,30 @@ package de.torbilicious.linkchecker
 import de.torbilicious.linkchecker.checker.LinkChecker
 import de.torbilicious.linkchecker.config.ConfigLoader
 import de.torbilicious.linkchecker.notification.DesktopNotifier
+import de.torbilicious.linkchecker.notification.NotificationType
 import de.torbilicious.linkchecker.site.Site
+import de.torbilicious.linkchecker.site.SiteState.DOWN
+import de.torbilicious.linkchecker.site.SiteState.UP
 import java.net.URL
 
-fun main(args: Array<String>) {
-    val notifier = DesktopNotifier()
+val checker = LinkChecker()
+val notifier = DesktopNotifier()
 
+fun main(args: Array<String>) {
     val sites = ConfigLoader().getConfig().sites.map { Site(URL(it)) }
 
-    val checker = LinkChecker()
+    var running = true
 
+    while (running) {
+        checkSites(sites)
+
+        Thread.sleep(10000)
+
+//        running = false
+    }
+}
+
+fun checkSites(sites: List<Site>) {
     sites.forEach {
         println("checking ${it.url}")
 
@@ -20,18 +34,13 @@ fun main(args: Array<String>) {
 
         println("new: ${it.state}")
         println("old: ${it.previousState}")
+
+        if (it.state == DOWN && it.previousState == UP) {
+            notifier.show("${it.url} is down!", duration = 5000, type = NotificationType.WARNING)
+        } else if (it.state == UP && it.previousState == DOWN) {
+            notifier.show("${it.url} is up again!", duration = 5000)
+        }
     }
 
-    Thread.sleep(1500)
-
-    sites.forEach {
-        println("checking ${it.url}")
-
-        it.state = checker.check(it)
-
-        println("new: ${it.state}")
-        println("old: ${it.previousState}")
-    }
-
-    notifier.show("Test notification")
+    println("Checked ${sites.size} site(s)")
 }
--- src/main/kotlin/de/torbilicious.linkchecker/checker/LinkChecker.kt
+++ src/main/kotlin/de/torbilicious.linkchecker/checker/LinkChecker.kt
@@ -1,19 +1,60 @@
 package de.torbilicious.linkchecker.checker
 
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
 import de.torbilicious.linkchecker.site.Site
 import de.torbilicious.linkchecker.site.SiteState
 import de.torbilicious.linkchecker.site.SiteState.DOWN
 import de.torbilicious.linkchecker.site.SiteState.UP
-import khttp.get
+import java.io.File
+import java.io.FileInputStream
+import java.security.KeyStore
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
+import java.util.*
+
 
 class LinkChecker {
-    fun check(site: Site): SiteState {
-        val code = get(site.url.toString()).statusCode
+    init {
+        val cert = getPem()
+        if (cert != null) {
+            if (FuelManager.instance.keystore == null) {
+                val ks = loadDefaultKeystore()
+                ks?.setCertificateEntry(UUID.randomUUID().toString(), cert)
 
-        return if (code == 200) {
+                FuelManager.instance.keystore = loadDefaultKeystore()
+            }
+
+        }
+    }
+
+    fun check(site: Site): SiteState {
+        val (_, response) = Fuel.get(site.url.toString()).responseString()
+
+        return if (response.statusCode == 200) {
             UP
         } else {
             DOWN
         }
     }
+}
+
+fun getPem(): Certificate? {
+    val pemStream = LinkChecker::class.java.
+            classLoader.
+            getResource("pflive.pem").openStream()
+
+
+    val fact = CertificateFactory.getInstance("X.509")
+    return fact.generateCertificate(pemStream)
+}
+
+fun loadDefaultKeystore(): KeyStore? {
+    val filename = System.getProperty("java.home") + "/lib/security/cacerts".replace('/', File.separatorChar)
+    val `is` = FileInputStream(filename)
+    val keystore = KeyStore.getInstance(KeyStore.getDefaultType())
+    val password = "changeit"
+    keystore.load(`is`, password.toCharArray())
+
+    return keystore
 }
